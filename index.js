e2230a87a3d82d06f29f88c928f98a50ad4bc0aa
AnskuBlomqvist <ansku.blomqvist@student.hamk.fi>
2023-04-20 17:54:13 +0300

    Valmis?

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-// Simple dataset
-let products = [
+// "Simple" dataset
+let plants = [
   { id: 1, name: 'Palmuvehka', latinName: 'Zamioculcas zamiifolia', colors: ['vihreä','musta'], flowers: false },
   { id: 2, name: 'Kiinanruusu', latinName: 'Hibiscus', colors: ['vihreä','punainen'], flowers: true },
   { id: 3, name: 'Neulavehka', latinName: 'Rhaphidophora tetrasperma', colors: 'vihreä', flowers: false },
@@ -32,6 +34,68 @@ app.get('/', (req,res) => {
     plants: plants
   });
 })
+//GET ONE
+app.get('/:id', (req,res ) => {
+  const id = Number(req.params.id);
+  const plant = plants.find(plant => plant.id === id);
+  if (plant)
+  {
+    res.json(plant);
+  }
+  else
+  {
+    res.status(404).json(
+      {
+        msg: "Not found"
+      }
+    );
+  }
+});
+//DELETE
+app.delete('/:id', (req,res) => {
+  const id = Number(req.params.id);
+  plants = plants.filter(plant => plant.id != id);
+  res.json(plants)
+
+});
+
+//CREATE
+app.post('/', (req,res)=> {
+  const newId = plants[plants.length-1].id + 1;
+  const newPlant = {
+    id : newId,
+    name : req.body.name,
+    latinName : req.body.latinName,
+    colors : req.body.colors,
+    flowers : req.body.flowers
+  }
+  plants.push(newPlant);
+  res.json(plants);
+})
+
+//UPDATE
+app.patch('/:id', (req, res) => {
+  const idToUpdate = Number(req.params.id);
+  const newName = req.body.name;
+  const newlatinName = req.body.latinName;
+  const newColors = req.body.colors;
+  const newFlowers = req.body.flowers;
+  console.log(newName);
+  console.log(newlatinName);
+  res.send("ok still going strong");
+
+  plants.forEach(plant => {
+    if (plant.id === idToUpdate)
+    {
+      plant.name = newName;
+      plant.latinName = newlatinName;
+      plant.colors = newColors;
+      plant.flowers = newFlowers;
+    }
+  })
+  res.json(plants);
+
+  });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening port ${PORT}`));
